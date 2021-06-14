a7cec95d3aeb7e926b53717dda08ace05a5faf81
dinushchathurya <dinushchathurya21@gmail.com>
2021-06-14 21:14:31 +0530

    fix: add missing Class method to aliases(#92c2551)

--- config/app.php
+++ config/app.php
@@ -226,7 +226,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'Location' => 'Stevebauman\Location\Facades\Location',
+        'Location' => Stevebauman\Location\Facades\Location::class,
     ],
 
 ];
